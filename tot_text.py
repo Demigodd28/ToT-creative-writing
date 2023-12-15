cd52323dd53c6b39467a30cffe86920e70f9a52b
Demigodd28 <jesse920208@gmail.com>
2023-12-15 23:46:43 +0800

    version 12/15

--- tot_text.py
+++ tot_text.py
@@ -1,26 +1,51 @@
 from llama_cpp import Llama
 from tot.prompts.text import *
-import os
+import parameters
+
+llm = Llama(
+    model_path = "openhermes-2.5-neural-chat-7b-v3-1-7b.Q2_K.gguf",
+    n_ctx=2048,
+    # n_gpu_layers=-1
+)
 
 def Generator(llm, node):
     new_node ={}
     output = []
 
-    prompt = cot_prompt.format(input = node[0] + node[1] + node[2] + node[3])
+    prompt = cot_prompt.format(input = node[1]['answer'][0] + node[1]['answer'][1] + node[1]['answer'][2] + node[1]['answer'][3])
 
-    for i in range(5):
-        new_node['id'] = id
-        new_node['answer'] = llm(
-                                prompt,
-                                max_tokens = 2048,
-                                echo = False
-                            )
+    for _ in range(5):
+        ans_from_llm = {}
+        filtered_ans = ""
+        if node[0] == None:
+            ans_from_llm = llm(
+                            prompt,
+                            max_tokens = 2048,
+                            stop=["\n\n", "known"],
+                            echo = False)
+            ans_from_llm = ans_from_llm["choices"][0]["text"].split('Passage:')##left [plan:1.2.3.4., paragragh*4]
+            filtered_ans = ans_from_llm[0]##chose [0]
+        else:
+            prompt += f"""the plan has been chose to be {node[0][0]['answer']}, then only write the passage according to the plan. Your output should be of the following format:
+                    Passage:
+                    Your passage here.
+                    """
+
+            ans_from_llm = llm(
+                            prompt,
+                            max_tokens = 2048,
+                            stop=["\n\n", "known"],
+                            echo = False)
+            filtered_ans = ans_from_llm["choices"][0]["text"]##left paragragh*4
+       
+        new_node['id'] = parameters.id
+        parameters.increase_id()
+        new_node['answer'] = [filtered_ans]
         new_node['value'] = None
-        new_node['parent_node'] = current_node['id']
+        new_node['parent_node'] = node[1]['id']
         new_node['ancester_value'] = None
 
         output.append(new_node)
-        id += 1
     return output
 
         
@@ -28,16 +53,20 @@ def Generator(llm, node):
 def Evaluator(llm, node):#node = []
     new_node = {}
     output = []
+    best = []
+    ans_from_llm = {}
 
-    prompt  = vote_prompt + 'Choices: ' + node[0] + node[1] + node[2] + node[3] + node[4]
+    prompt  = vote_prompt + 'Choices: ' + node[0]['answer'][0] + node[1]['answer'][0] + node[2]['answer'][0] + node[3]['answer'][0] + node[4]['answer'][0]
 
-    best = llm(
+    ans_from_llm = llm(
                 prompt,
                 max_tokens = 2048,
+                stop=["\n\n", "known"],
                 echo = False
             )
+    best = ans_from_llm["choices"][0]["text"].split()
     for i in range(5):
-        if best == node[i]['id']:
+        if best[4] == str(node[i]['id']):
             break
     new_node = node[i] 
     output.append(new_node)
@@ -45,32 +74,26 @@ def Evaluator(llm, node):#node = []
 
 
 if __name__ == '__main__':
-    llm = Llama(
-        model_path=".\models\openhermes-2.5-neural-chat-7b-v3-1-7b.Q8_0.gguf", #model的路徑(要包含副檔名)
-        n_ctx=2048, #text的長度上限，llama-cpp的預設上限為2048，但default為512
-        n_gpu_layers=-1, #要分擔給GPU的layer數量，-1為全部
-    )
 
     question = []  #input 4 sentence
-    a = input()
-    b = input()
-    c = input()
-    d = input()
+    a = "It isn't difficult to do a handstand if you just stand on your hands."
+    b = "It caught him off guard that space smelled of seared steak."
+    c = "When she didn't like a guy who was trying to pick her up, she started using sign language."
+    d = "Each person who knows you has a different perception of who you are."
     question.extend([a, b, c, d])
 
-    root_node = {'id':0,
+    root_node = {'id':parameters.id,
                 'answer':question,
                 'value':None,
                 'parent_node':None,
                 'ancester_value':None
                 }
+    parameters.increase_id()
 
-    current_node = root_node
-    id = 1
-    writing_plans = Generator(llm, question)###不需要plan以外的文字
+    writing_plans = Generator(llm, [None, root_node])### Generator(llm, [plan, root_node])  no plan -->None
     best_plan = Evaluator(llm, writing_plans)
 
-    current_node = best_plan[0]
-    id = 6
-    passages = Generator(llm, question)### 已經有plan 了，不需要passage以外的文字
-    best_passage = Evaluator(llm, passages) 
+    passages = Generator(llm, [best_plan, root_node])
+    best_passage = Evaluator(llm, passages)
+    print(best_plan[0]['answer'])
+    print(best_passage[0]['answer'])
