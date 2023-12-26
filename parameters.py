d6036dd2d39b28527d6400d4c88c535985b1e7f3
Demigodd28 <jesse920208@gmail.com>
2023-12-26 15:01:46 +0800

    version 12/26

--- parameters.py
+++ parameters.py
@@ -3,3 +3,25 @@ id = 0
 def increase_id():
     global id  # 明確指定訪問全局變數 id
     id += 1
+
+
+cot_prompt_1 = '''
+Make a coherent plan of the four sentences: {input}.
+Your output should be of the following format:
+
+Plan:
+Your plan here.
+
+'''
+
+cot_prompt_2 = '''
+Write a coherent passage of 4 short paragraphs in topic of: {plan}. The end sentence of each paragraph must be: {input}
+
+ Your output should be of the following format:
+
+Passage:
+Your passage here.
+'''
+
+vote_prompt = '''Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line "The best choice is {s}", where s the integer id of the choice.
+'''
