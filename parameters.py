930a3d2cdcec2cd6003f41aecbbe152ba6f5f511
Demigodd28 <jesse920208@gmail.com>
2024-01-03 15:50:45 +0800

    version 1/3

--- parameters.py
+++ parameters.py
@@ -17,11 +17,15 @@ Your plan here.
 cot_prompt_2 = '''
 Write a coherent passage of 4 short paragraphs in topic of: {plan}. The end sentence of each paragraph must be: {input}
 
- Your output should be of the following format:
+Your output should be of the following format:
 
 Passage:
 Your passage here.
 '''
 
-vote_prompt = '''Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line "The best choice is {s}", where s the integer id of the choice.
+vote_prompt = '''Given an instruction and several choices, decide which choice is most promising. Analyze each choice in detail, then conclude in the last line.
+
+Your output should be of the following format:
+
+"The best choice is {s}", where s the integer id of the choice.
 '''
